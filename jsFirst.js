8398b455be71cfc3077f3edf3bafe33f9e82a7f2
shovon56 <shovon@medinatech.co>
2021-09-06 03:39:14 +0600

    js

--- jsFirst.js
+++ jsFirst.js
@@ -119,3 +119,65 @@ car.color = "red";
 car.owner = "Johnson";
 
 
+/// Function
+
+let x = myFunction(4, 3);   // here 4,3 are arguments
+
+function myFunction(a, b) { // here a,b are parameters
+  return a * b;             // Function returns the product of a and b
+}
+
+/// Basic Object
+
+
+//**** this is used not to call same object again and again *
+//**** inside the fullName value : here person.firstName == this.firstName *
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+
+// Use of back slash escape
+
+let text = "We are the so-called \"Vikings\" from the north.";
+document.getElementById("demo").innerHTML = text; 
+
+// String
+
+let x = "John";
+let y = new String("John");
+
+// typeof x will return string
+// typeof y will return object
+
+
+
+let x = "John";             
+let y = new String("John");
+
+// (x == y) is true because x and y have equal val
+
+
+let x = "John";             
+let y = new String("John");
+
+// (x === y) is false because x and y have different types (string and object)
+
+/// Two objects cant be same in value and type
+
+let x = new String("John");             
+let y = new String("John");
+
+// (x == y) is false because x and y are objects
+
+let x = new String("John");             
+let y = new String("John");
+
+// (x === y) is false because x and y are objects
+
+
