48cdfff67be38bbadf325dd1401019e36b6c871c
shovon56 <shovon@medinatech.co>
2022-04-22 02:35:05 +0600

    array method start

---
+++ arrayMethods.js
@@ -0,0 +1,12 @@
+
+// for each implementation
+
+let loop =  () => {
+    const months = ['January', 'February', 'March', 'April'];
+
+        months.forEach(function(month) {
+        console.log(month);
+        });
+};
+
+ loop();
